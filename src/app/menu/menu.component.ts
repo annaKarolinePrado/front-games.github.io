bce431c3a71f93b25445171bff181d8b072b3e9b
Anna Prado <annakaroline9298@gmail.com>
2024-07-11 18:01:48 -0300

    Criado menu principal da aplicação.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent { }
+export class MenuComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    const nav = document.querySelector('nav');
+    const sidebar = document.querySelector('.sidebar');
+    if (nav && !nav.contains(target) && sidebar && !sidebar.contains(target) && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
+}
