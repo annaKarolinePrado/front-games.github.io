4acc3769126fdc2f553cabec9235ccceec393f80
Anna Prado <annakaroline9298@gmail.com>
2024-07-12 16:40:26 -0300

    Adiciona variável de cor padrão para inputs no CSS global; inclui Login no menu; cria componente de Login.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, MenuComponent, RodapeComponent],
+  imports: [RouterModule, MenuComponent, RodapeComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
