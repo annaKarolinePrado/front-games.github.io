eea55a7bf7b164dd87447e87e12eeb7167996466
Anna Prado <annakaroline9298@gmail.com>
2024-07-16 14:57:44 -0300

    #TR-02: Criado alternância entre formulários de login e cadastro ao clicar no link.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,10 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+  showSignupForm(event: Event) {
+    event.preventDefault();
+    const loginContainer = document.getElementById('login-container');
+    const signupContainer = document.getElementById('signup-container');
+    if (loginContainer && signupContainer) {
+      loginContainer.style.display = 'none';
+      signupContainer.style.display = 'block';
+    }
+  }
+
+  showLoginForm(event: Event) {
+    event.preventDefault();
+    const loginContainer = document.getElementById('login-container');
+    const signupContainer = document.getElementById('signup-container');
+    if (loginContainer && signupContainer) {
+      loginContainer.style.display = 'block';
+      signupContainer.style.display = 'none';
+    }
+  }
 }
