d127d370e80baef84dbff0edd92958bb826e39b9
Anna Prado <annakaroline9298@gmail.com>
2024-07-18 11:40:56 -0300

    #TR-02: Refatorado cadastro de usuário e criado componente singup.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,15 +7,14 @@ import { UserDTO } from '../models/user.dto';
 
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
 })
-export class LoginComponent {
-
-  
+export class SignupComponent {
+ 
   signupForm: FormGroup;
   showSignupForm: boolean = true;
 
