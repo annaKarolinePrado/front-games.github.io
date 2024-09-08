878e9a19ad567c8111b767d48cae5804bda0a85a
Anna Prado <annakaroline9298@gmail.com>
2024-09-08 19:33:25 -0300

    Implementado recurso para selecionar um jogo da memória aleatoriamente.

--- src/app/home-game/jogo-da-memoria/jogo-da-memoria.component.ts
+++ src/app/home-game/jogo-da-memoria/jogo-da-memoria.component.ts
@@ -12,12 +12,28 @@ export class JogoDaMemoriaComponent {
 
   constructor(private router: Router) {}
 
-  goBack(): void {
-    this.router.navigate(['']);
-  }
-
   showGameFree(): void {
     this.router.navigate(['home-game/jogo-da-memoria/game-board-free/available-boards']);
+  }
+
+  temas = [
+    { path: 'home-game/jogo-da-memoria/game-board-free/animals' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/emojis-board' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/flower-board' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/fruit-board' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/hand-board' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/negative-number-board' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/symbols' },
+    { path: 'home-game/jogo-da-memoria/game-board-free/traffic-signs' },
+  ];
+
+  escolherTemaAleatorio() {
+    const temaAleatorio = this.temas[Math.floor(Math.random() * this.temas.length)];
+    this.router.navigate([temaAleatorio.path]);
+  }
+
+  goBack(): void {
+    this.router.navigate(['']);
   }
 
 }
