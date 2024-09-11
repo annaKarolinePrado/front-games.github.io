60ce936efe3128a51efbeba0358d52042a507725
Anna Prado <annakaroline9298@gmail.com>
2024-09-11 16:13:33 -0300

    Implementado recurso para enviar um argumento pela url para ser recebido por outro componente, e usá-lo para definir os níveis fáceis, médio e difícil.

--- src/app/home-game/jogo-da-forca/jogo-da-forca.component.ts
+++ src/app/home-game/jogo-da-forca/jogo-da-forca.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SimpleWordsBoardComponent } from './forca-board/simple-words-board/simple-words-board.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-jogo-da-forca',
-  standalone: true,
-  imports: [ SimpleWordsBoardComponent ],
-  templateUrl: './jogo-da-forca.component.html',
-  styleUrl: './jogo-da-forca.component.css'
+    selector: 'app-jogo-da-forca',
+    standalone: true,
+    imports: [ SimpleWordsBoardComponent ],
+    templateUrl: './jogo-da-forca.component.html',
+    styleUrl: './jogo-da-forca.component.css'
 })
+
 export class JogoDaForcaComponent {
-  constructor(private router: Router) {}
+    constructor(private router: Router) {}
+    
+    showGameAleatorio(nivel: string) {
+        this.router.navigate(['/home-game/jogo-da-forca/forca-board/simple-words-board'], { queryParams: { nivel: nivel } });
+    }
 
-  showGameAleatorio(): void {
-    this.router.navigate(['home-game/jogo-da-forca/forca-board/simple-words-board']);
-  }
-
-
-  goBack(): void {
-    this.router.navigate(['']);
-  }
+    goBack(): void {
+        this.router.navigate(['']);
+    }
 }
